cff7e2d6a0a679fa3682d9d588c2cb5640e8b5f6
Quintez Martin <mrmartin2016@yahoo.com>
2018-02-10 02:47:46 -0500

    Greeter fix

--- media/greeter.js
+++ media/greeter.js
@@ -4,40 +4,27 @@ alert('Welcome to my petty page!');
 
 var name = prompt('What is your name?');
 
-// var checkName = function(){
-//   if(name === null) {
-//     alert("You didn't put a name.");
-// } else if(name === "") {
-//   name = prompt('What is your name, for real this time?');
-//
-// } else {
-//   alert('Hello ' + name + ".");
-//   }
-// }
-
-// var checkName = function(){
-
-if (name != null) {
-  console.log("null");
-} else if (name === "") {
-  name = prompt('What is your name, for real this time?');
-} else {
-  alert('Hello ' + name + ".");
+var checkName = function() {
+  if (name === "" || name === "null") {
+    name = prompt('What is your name, for real this time?');
+    checkName();
+  } else {
+    alert('Hello ' + name + ".");
   }
-// }
-// checkName(); // kicks off the name-checking the first time
+}
 
-// alert('Hello ' + name + ".");
+checkName(); // kicks off the name-checking the first time
 
 var age = prompt('How old are you ' + name + '?');
 
 var checkAge = function() {
   if (!parseInt(age, 10)) {
-    return prompt ("C'mon, just tell us your age!");
-
+    age = prompt ("C'mon, just tell us your age!");
     checkAge();
   } // Check to see if input is not a number
-   return age = alert('Great ' + name + '! You are ' + age + ' years old!');
+  else {
+    alert('Great ' + name + '! You are ' + age + ' years old!');
+  }
 }
 
 checkAge();
